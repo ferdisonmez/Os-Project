33e2f030ca92592be87867afbc93a8a2159b450a
Ferdi Sönmez <36517990+ferdisonmez@users.noreply.github.com>
2021-12-04 13:59:28 -0800

    Update Main.c
    Kod düzenlendi.

--- Main.c
+++ Main.c
@@ -5,7 +5,8 @@
 #include<signal.h>
 #include<sys/types.h>
 #include <sys/wait.h>
-
+#define MAX_COMMAND_CH 80
+#define MAX_COMMAND_WORD 10
 
 #define RED   "\x1B[31m"
 #define GRN   "\x1B[32m"
@@ -18,8 +19,6 @@
 
 static char* currentDirectory;
 extern char** environ;
-
-
 
 char * kWord[] = {
 	"exit",
@@ -37,9 +36,9 @@ char * kWord[] = {
 
 
 
-void welcomeScreen(){
+void SauShell(){
         printf("\n\t============================================\n");
-        printf("\t                   C Shell  \n");
+        printf("\t                   Saü Shell  \n");
         printf("\t--------------------------------------------\n");
         printf("\t                                  \n");
         printf("\t============================================\n");
@@ -67,14 +66,12 @@ void controlCommand(char ** commandStr){
 
 }
 
-
-
 void execute(char ** commandStr){
 	int pid;
 	pid = fork();
 
 	if(pid < 0){
-		printf("fork() return error \n");
+		printf("fork() olusturulamadi\n");
 		exit(-1);
 	}
 
@@ -84,59 +81,110 @@ void execute(char ** commandStr){
 		controlCommand(commandStr);
 
 		if ( execvp(commandStr[0] , commandStr) == -1){
-			printf("execvp error is occured\n");
+			printf("execvp hatası olustu\n");
 			exit(0);
-			//kill(getpid() , SIGTERM );
 		}
 	}
 	else{
 			
-		waitpid(-1,NULL,0);
-			
+		waitpid(-1,NULL,0);	
 	}
 
 
 }
-
-
 void  parse(char *line, char **argv)
 {
-     while (*line != '\0') {       /* if not the end of line ....... */ 
+     while (*line != '\0') {      
           while (*line == ' ' || *line == '\t' || *line == '\n')
-               *line++ = '\0';     /* replace white spaces with 0    */
-          *argv++ = line;          /* save the argument position     */
+               *line++ = '\0';     
+          *argv++ = line;          
           while (*line != '\0' && *line != ' ' && 
                  *line != '\t' && *line != '\n') 
-               line++;             /* skip the argument until ...    */
+               line++;             
      }
-     *argv = '\0';                 /* mark the end of argument list  */
+     *argv = '\0';                 
+}
+
+char** parseLine(int *numberOfWord, char *line, char *argv[])
+{
+
+   int i=0;
+   *numberOfWord=0;
+   const char space[2] = " ";
+   char *word;
+   
+
+   if(line[0]=='\0'){
+        printf("Parametre Yok\n");
+        return NULL;
+   }
+   else{
+    
+   word = strtok(line, space);
+   
+ 
+   argv[i]=(char *)malloc((MAX_COMMAND_CH+1)*sizeof(char));
+   strcpy(argv[i],word);
+
+   i++;
+   *numberOfWord=i;
+   while(word!= NULL) {
+   
+      word = strtok(NULL, space);
+    
+      if(word==NULL){
+        break;
+      }
+
+      argv[i]=(char *)malloc((MAX_COMMAND_CH+1)*sizeof(char));
+      strcpy(argv[i],word);
+           
+      i++;
+      *numberOfWord=i;
+      if(*numberOfWord>10){*numberOfWord=0; printf("Many parameter\n");return NULL;}
+
+   }  
+   }   
+    
+   return argv;
+
 }
 
 
-void PromptBas()
+
+void PromptYazdir()
 {
+    char* username = getenv("USER");
     char hostn[1204] = "";
     gethostname(hostn, sizeof(hostn));
-    printf(YEL "%s@%s:"RESET RED "%s > " RESET, getenv("LOGNAME"), hostn, getcwd(currentDirectory, 1024));
+    printf(YEL "%s@%s:"RESET RED "%s > " RESET,username , hostn, getcwd(currentDirectory, 1024));
 }
 
-int main(int argc,char ** argv,char ** envp){
-
+int main(int argc,char ** envp){
 	environ=envp;
-	welcomeScreen();
+	SauShell();
 
+    char line[MAX_COMMAND_CH+1]; //Max numbers of character
+    char *argv[MAX_COMMAND_CH+1];  /* prompt argümanları/parametreleri     */
+	char** myArgV=(char**)malloc(sizeof(char*)*10);
+    myArgV[0]="\0";
+    int wordNum=0;
+    int j=0;
 
-	char * line = (char *)calloc(1024,sizeof(char));
-	
-	int * childReturn = (int*) calloc(1,sizeof(int));
-	
-	char * myArgV[80] /*= {"pwd",NULL}*/;
 	while(1){
 
-		PromptBas();
-		printf("sau -> ");     /*   display a prompt             */
-          gets(line);              /*   read in the command line     */
-          parse(line, myArgV);       /*   parse the line               */
+		PromptYazdir();
+		printf("Sau :");  
+
+    if (fgets(line, sizeof(line), stdin)!=NULL) {    
+        line[strcspn(line, "\n")] = '\0';        
+    }   
+                  
+    myArgV=parseLine(&wordNum,line,argv);
+    memset(line,'\0',MAX_COMMAND_CH*sizeof(char));
+
+       //   gets(line);             
+        //  parse(line, myArgV);      
 		
 
 		if( strcmp(myArgV[0],kWord[0] ) == 0){
@@ -156,9 +204,13 @@ int main(int argc,char ** argv,char ** envp){
 
 		execute(myArgV);
 
-
+          for ( j = 0;j<wordNum; j++)
+          {
+             free(myArgV[j]);
+          }
+        
 	}
-
+    
 	return 0;
 
 
